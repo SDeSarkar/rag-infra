10505b108312732869826db860963ec3f4d47db9
SameepSB <sameeplcm@gmail.com>
2026-02-23 23:57:05 +0530

    Create redis.tf

---
+++ terraform/redis.tf
@@ -0,0 +1,14 @@
+resource "azurerm_redis_cache" "redis" {
+  name                = "${var.project}-${var.env}-redis-${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  family              = "C"
+  sku_name            = "Basic"
+  capacity            = 0
+
+  enable_non_ssl_port = false
+  minimum_tls_version = "1.2"
+
+  tags = var.tags
+}
