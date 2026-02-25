867d59fc3a00b41a2720d9f02d99f1710a2e4157
SameepSB <sameeplcm@gmail.com>
2026-02-25 13:29:56 +0530

    Create monitoring.tf

---
+++ terraform/monitoring.tf
@@ -0,0 +1,21 @@
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.project}-${var.env}-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku               = "PerGB2018"
+  retention_in_days = 30
+
+  tags = var.tags
+}
+
+resource "azurerm_application_insights" "ai" {
+  name                = "${var.project}-${var.env}-appi"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.law.id
+
+  tags = var.tags
+}
