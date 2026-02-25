277febae950a3bc4bfa3913f1820c0485716e650
SameepSB <sameeplcm@gmail.com>
2026-02-25 13:29:16 +0530

    Create acr.tf

---
+++ terraform/acr.tf
@@ -0,0 +1,10 @@
+resource "azurerm_container_registry" "acr" {
+  name                = replace("${var.project}${var.env}${random_string.suffix.result}", "-", "")
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku           = "Basic"
+  admin_enabled = true # MVP. Production: prefer Managed Identity + AcrPull.
+
+  tags = var.tags
+}
