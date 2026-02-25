7d270aa14d08e96701ae0a5cd24f6c750049a8e7
sameepSB <sameeplcm@gmail.com>
2026-02-25 08:08:41 +0000

    terraform fmt

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -14,8 +14,8 @@ resource "azurerm_application_insights" "ai" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.law.id
+  application_type = "web"
+  workspace_id     = azurerm_log_analytics_workspace.law.id
 
   tags = var.tags
 }
