7279c189f19523306ecafc7d1b315cfdf33ceeae
SameepSB <sameeplcm@gmail.com>
2026-03-05 13:24:36 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
     resource_group {
@@ -5,3 +22,5 @@ provider "azurerm" {
     }
   }
 }
+
+provider "azuread" {}
